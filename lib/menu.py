d78f6282c6cb86ba55c38c4ccbbc6f692e63ab3a
Anthony Samartsev <kycok@archlinux.info>
2016-08-17 17:20:03 +0300

    изменения для использования python v3.5

--- lib/menu.py
+++ lib/menu.py
@@ -1,21 +1,30 @@
-#!/usr/bin/python
+#!/usr/bin/python3.5
 # -*- coding: utf-8 -*-
 
 class Menu():
+	'''Стандартный класс меню'''
 	def __init__(self):
+		'''Инициализация: присвоение заголовка и списка пунктов'''
 		self.Header   = 'Морской бой!'
-		self.Menu = ('Выход',
+		self.Menu = ['Выход',
 					 'Начать игру',
 					 'Справка',
 					 'История',
-					 'Планы')
+					 'Планы']
 	def render(self):
+		'''Отображение меню на экране'''
 		print (self.Header)
 		print ('-------------------------------')
-		for i in range (1, len (self.Menu)) + [0]:
+		for i in list (range (1, len (self.Menu))) + [0]:
 			print ("%d. %s" % (i, self.Menu [i]))
-		index = input ('Что делать? ')
-		if index in range(len(self.Menu)):
+		return self.user_input()
+	def user_input(self):
+		'''Получение номера выбранного пункта'''
+		try:
+			index = int (input ('Что делать? '))
+		except:
+			return None
+		if index in tuple (range (len (self.Menu))):
 			return self.Menu[index]
 
 def _yes_no (Question):
