30e3003749f96ca8cc251f399a7a918074661cf1
Vlasov Anton <Vlasov-Ant@yandex.ru>
2017-03-27 20:20:11 +0300

    Добавление расстановки кораблей игроком

--- bin/start.py
+++ bin/start.py
@@ -20,7 +20,7 @@ path.append ('/data/git/Battleship/images')
 import pygame
 from color_vars   import BLACK, WHITE
 from draw_classes import Menu
-from game_classes import Player
+from game_classes import Player, Field
 from main_vars    import VERSION
 from screen_vars  import SCREEN_SIZE
 
@@ -46,11 +46,55 @@ while current_status != 'quit':
         if   event.key == pygame.K_DOWN: menu.moving ('next')
         elif event.key == pygame.K_UP:   menu.moving ('prev')
         elif event.key == 13:            current_status = menu.get_status ()
+      elif current_status == 'set_ships':
+        if player.is_active:
+          for i in range (len (player.list_ships)):
+            ship = player.list_ships [i]
+            if not ship.is_set:
+              if   event.key == pygame.K_LEFT:  ship.start_cell [0] -= 1
+              elif event.key == pygame.K_RIGHT: ship.start_cell [0] += 1
+              elif event.key == pygame.K_UP:    ship.start_cell [1] -= 1
+              elif event.key == pygame.K_DOWN:  ship.start_cell [1] += 1
+              elif event.key == pygame.K_SPACE:
+                if    ship.orient == 'horizont': ship.orient = 'vertical'
+                else:                            ship.orient = 'horizont'
+
+              if   ship.start_cell [0] < 0: ship.start_cell [0] = 0
+              elif ship.start_cell [0] > Field.SIZE ['x'] - ship.length and ship.orient == 'horizont': ship.start_cell [0] = Field.SIZE ['x'] - ship.length
+              elif ship.start_cell [0] > Field.SIZE ['x'] - 1           and ship.orient == 'vertical': ship.start_cell [0] = Field.SIZE ['x'] - 1
+
+              if   ship.start_cell [1] < 0: ship.start_cell [1] = 0
+              elif ship.start_cell [1] > Field.SIZE ['y'] - ship.length and ship.orient == 'vertical': ship.start_cell [1] = Field.SIZE ['y'] - ship.length
+              elif ship.start_cell [1] > Field.SIZE ['y'] - 1           and ship.orient == 'horizont': ship.start_cell [1] = Field.SIZE ['y'] - 1
+
+              ship.update_set_status (player.field.list_cells)
+              if event.key == 13:
+                if ship.set_status:
+                  ship.is_set = True
+                  player.field.update (ship.oreol,      'oreol')
+                  player.field.update (ship.list_cells, 'ship')
+
+              player.list_ships [i] = ship
+              break
 
   if current_status == 'new_game':
     player = Player ('human')
+    player.is_active = True
+    for koord in player.field.list_cells:
+      cell = player.field.list_cells [koord]
+      cell.is_visible = True
     comp   = Player ('AI')
     current_status = 'set_ships'
+
+  if current_status == 'set_ships':
+    is_set   = True
+    for ship in player.list_ships:
+      if not ship.is_set:
+        is_set = False
+        break
+    for ship in comp.list_ships:
+      if not ship.is_set:
+        is_set = False
 
   screen.fill (WHITE)
 
@@ -59,6 +103,12 @@ while current_status != 'quit':
   elif current_status == 'set_ships':
     player.field.draw (screen,  10, 10)
     comp.field.draw   (screen, 320, 10)
+    for ship in player.list_ships:
+      if ship.is_set: ship.draw (screen, player.type)
+      else:
+        ship.update_set_status (player.field.list_cells)
+        ship.draw (screen, player.type)
+        break
 
   pygame.display.flip ()
 
--- lib/game_classes.py
+++ lib/game_classes.py
@@ -1,12 +1,17 @@
 #!/bin/python3
 
 import pygame
-from color_vars import WHITE, BLACK
+from color_vars import WHITE, BLACK, RED, BLUE, GREEN
 
 class Cell (object):
   '''Ячейка поля'''
   STATUSES = {
-    'clear': WHITE,
+    'clear':   WHITE,
+    'wounded': RED,
+    'kill':    BLUE,
+    'miss':    BLACK,
+    'ship':    RED,
+    'oreol':   BLUE,
   }
 
   SIZE = {
@@ -15,10 +20,12 @@ class Cell (object):
   }
 
   def __init__ (self):
+    self.is_visible = False
     self.status = 'clear'
 
   def draw (self, screen, x, y):
     pygame.draw.rect (screen, BLACK, [x, y, self.SIZE ['x'], self.SIZE ['y']], 1)
+    if self.is_visible: pygame.draw.rect (screen, self.STATUSES [self.status], [x + 1, y + 1, self.SIZE ['x'] - 2, self.SIZE ['y'] - 2])
     
 class Field (object):
   '''Игровое поле'''
@@ -38,11 +45,78 @@ class Field (object):
       cell = self.list_cells [(x_cell, y_cell)]
       cell.draw (screen, x + x_cell * cell.SIZE ['x'], y + y_cell * cell.SIZE ['y'])
 
+  def update (self, list_cells, status):
+    for cell in list_cells:
+      if cell in self.list_cells: self.list_cells [cell].status = status
+
+class Ship (object):
+  '''Корабль'''
+  def __init__ (self, length):
+    self.length      = length
+    self.start_cell  = [4, 4]
+    self.orient      = 'horizont'
+    self.oreol       = []
+    self.list_cells  = []
+    self.is_set      = False
+    self.list_damage = []
+    self.set_status  = True
+
+  def draw (self, screen, type):
+    if self.orient == 'horizont':
+      if type == 'human':
+        if self.set_status: color = GREEN
+        else:               color = RED
+        pygame.draw.rect (screen, color, [15 + self.start_cell [0] * Cell.SIZE ['x'], 15 + self.start_cell [1] * Cell.SIZE ['y'], Cell.SIZE ['x'] * self.length - 10, Cell.SIZE ['y'] - 10])
+    else:
+      if type == 'human':
+        if self.set_status: color = GREEN
+        else:               color = RED
+        pygame.draw.rect (screen, color, [15 + self.start_cell [0] * Cell.SIZE ['x'], 15 + self.start_cell [1] * Cell.SIZE ['y'], Cell.SIZE ['x'] - 10, Cell.SIZE ['y'] * self.length - 10])
+
+  def update_set_status (self, list_cells):
+    self.set_status = True
+    start_x = self.start_cell [0] - 1
+    start_y = self.start_cell [1] - 1
+    end_x   = self.start_cell [0] + self.length + 1
+    end_y   = self.start_cell [1] + self.length + 1
+    if self.orient == 'horizont': end_y = self.start_cell [1] + 2
+    else:                         end_x = self.start_cell [0] + 2
+
+    self.oreol      = []
+    self.list_cells = []
+    if self.orient == 'horizont':
+      for x in range (self.start_cell [0], self.start_cell [0] + self.length):
+        self.list_cells.append ((x, self.start_cell [1]))
+    else:
+      for y in range (self.start_cell [1], self.start_cell [1] + self.length):
+        self.list_cells.append ((self.start_cell [0], y))
+    for y in range (start_y, end_y):
+      for x in range (start_x, end_x):
+        self.oreol.append ((x, y))
+        try:
+          cell = list_cells [(x, y)]
+        except:
+          cell = list_cells [(self.start_cell [0], self.start_cell [1])]
+        if cell.status == 'ship': self.set_status = False
+
 class Player (object):
   '''Игрок'''
   def __init__ (self, type):
     self.type  = type
     self.field = Field ()
+    self.list_ships = [
+      Ship (4),
+      Ship (3),
+      Ship (3),
+      Ship (2),
+      Ship (2),
+      Ship (2),
+      Ship (1),
+      Ship (1),
+      Ship (1),
+      Ship (1),
+    ]
+    self.is_active = False
 
 if __name__ == '__main__':
   print ('Модуль с игровыми классами')
